2ab9d46090ab7c65854fca83ea8785955ae8b9ac
Biohazard <qwerst13@gmail.com>
2021-03-04 18:27:08 +0300

    main blocks added

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,53 @@
+// import Swiper JS
+import Swiper from 'swiper/bundle';
+// import Swiper styles
+import 'swiper/swiper.scss';
+
 import '../scss/style.scss';
 
-console.log('Works!');
+// core version + navigation, pagination modules:
+import SwiperCore, { Navigation, Pagination } from 'swiper/core';
+
+// configure Swiper to use modules
+SwiperCore.use(Navigation, Pagination);
+
+//init swiper only on mobile devices
+if (window.innerWidth<768) {
+  const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    /*loop: true,*/
+    slidesPerView: 'auto',
+    spaceBetween: 16,
+    // Pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    }
+  });
+}
+
+/* if resize need
+if (window.innerWidth>768) {
+  swiper.destroy(false,true);// если 2-й аргумент true, обнуляются пользовательские стили
+}
+*/
+
+// show/hide functionality on brands elements
+document.querySelector('.swiper-container>.about__expand').addEventListener('click',(e)=>{
+  document.querySelector('.swiper-wrapper').classList.toggle('swiper-wrapper--full');
+  e.target.classList.toggle('about__expand--expanded');
+  if (document.querySelector('.swiper-wrapper').classList.contains('swiper-wrapper--full')){
+    e.target.innerHTML='Скрыть';
+  } else {
+    e.target.innerHTML='Показать все';
+  }
+});
+
+// show/hide menu
+document.querySelector('.button--menu').addEventListener('click',()=>{
+  document.querySelector('.menu').classList.add('menu--visible');
+});
+document.querySelector('.button--close').addEventListener('click',()=>{
+  document.querySelector('.menu').classList.remove('menu--visible');
+});
