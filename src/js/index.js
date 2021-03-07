5fd11b4ea7f90656acb0732e05150ba0c890e943
Biohazard <qwerst13@gmail.com>
2021-03-08 00:18:53 +0300

    actions added

--- src/js/index.js
+++ src/js/index.js
@@ -36,7 +36,6 @@ if (window.innerWidth>768) {
 // show/hide functionality on swiper elements
 document.querySelectorAll('.swiper-container>.about__expand').forEach((item)=>{
   item.addEventListener('click',(e)=>{
-    console.log(e.target);
     e.target.parentElement.querySelector('.swiper-wrapper').classList.toggle('swiper-wrapper--full');
     e.target.classList.toggle('about__expand--expanded');
     if (e.target.parentElement.querySelector('.swiper-wrapper').classList.contains('swiper-wrapper--full')){
@@ -47,10 +46,52 @@ document.querySelectorAll('.swiper-container>.about__expand').forEach((item)=>{
   });
 });
 
+document.querySelector('.about .about__expand').addEventListener('click',(e)=>{
+   document.querySelector('.about__text').classList.toggle('about__text--full');
+    e.target.classList.toggle('about__expand--expanded');
+    if (e.target.parentElement.querySelector('.about__text').classList.contains('about__text--full')){
+      e.target.innerHTML='Скрыть';
+    } else {
+      e.target.innerHTML='Читать далее';
+    }
+  });
+
+
+
+let menu = document.querySelector('.menu'),
+    overlay = document.querySelector('.overlay');
+
+
 // show/hide menu
-document.querySelector('.button--menu').addEventListener('click',()=>{
-  document.querySelector('.menu').classList.add('menu--visible');
+document.querySelector('.button--menu').addEventListener('click', openMenu);
+document.querySelector('.button--close').addEventListener('click', closeMenu);
+overlay.addEventListener('click', closeMenu);
+
+
+//menu-items
+document.querySelectorAll('.menu__item').forEach((item)=>{
+  item.addEventListener('click', (e)=>{
+    document.querySelector('.menu__item--current').classList.remove('menu__item--current');
+    e.currentTarget.classList.add('menu__item--current');
+  });
 });
-document.querySelector('.button--close').addEventListener('click',()=>{
-  document.querySelector('.menu').classList.remove('menu--visible');
+
+//slide-menu-items
+document.querySelectorAll('.slide-menu__item').forEach((item)=>{
+  item.addEventListener('click', (e)=>{
+    document.querySelector('.slide-menu__item--current').classList.remove('slide-menu__item--current');
+    e.currentTarget.classList.add('slide-menu__item--current');
+  });
 });
+
+
+
+
+function openMenu() {
+  menu.classList.add('menu--visible');
+  overlay.classList.add('overlay--on');
+}
+function closeMenu() {
+  menu.classList.remove('menu--visible');
+  overlay.classList.remove('overlay--on');
+}
